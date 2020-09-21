c6d51c6f650ab467b49d3f49cf0755ec24eec2d4
AcarMeel <acarmeel@gmail.com>
2020-09-20 20:27:21 -0600

    Selectors for Shop Page

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { selectShopCollection } from '../../redux/shop/shop.selectors'
+import { selectCollectionForPreview } from '../../redux/shop/shop.selectors'
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
@@ -17,6 +17,6 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectShopCollection
+    collections: selectCollectionForPreview
 })
 export default connect(mapStateToProps)(CollectionsOverview);
